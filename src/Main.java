f02b036b9c449def880fe10fd0e6d3e7343682a4
zaikadm <zaikadm@dtechs.io>
2023-04-08 17:43:05 +0300

    java homework fix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int initialAccount = 100;
-        int depositAmount  = 500;
+        int depositAmount  = 2000;
 
-
+        bonusAccount(initialAccount, depositAmount);
+    }
+    private static void bonusAccount(int initialAccount, int depositAmount) {
         if (depositAmount > 1000) {
             int bonus = depositAmount / 100;
             int finalAccount = initialAccount + bonus + depositAmount;
